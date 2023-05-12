a9819760cbb1ab9efe760deb1018049926208174
Anvesh Laptop 2 <atvanvesh@gmail.com>
2023-05-11 20:09:03 -0400

    fixeing bugs

--- client/js/createForumPost.js
+++ client/js/createForumPost.js
@@ -1,35 +1,22 @@
+import * as crud from "./crud.js";
 
-import { createPost, getAllPosts } from './forumCrud.js';
+const REDIRECT_URI = 'http://localhost:3001/client/groups.html';
 
-const REDIRECT_URI = 'http://127.0.0.1:5501/client/Home.html';
-
-const form = document.querySelector('form');
+const form = document.getElementById("form");
 
 form.addEventListener('submit', async (event) => {
   event.preventDefault();
+  console.log("yoooooo")
   const formData = new FormData(event.target);
   const postData = {
-    'artist-name': formData.get('artist-name'), //aspects that align with crud
-    'event-date': formData.get('event-date'),
-    'event-name': formData.get('event-name'),
-    'post-content': formData.get('post-content'),
-    'date-posted': formData.get('date-posted')
+    'artist_name': formData.get('artist-name'),
+    'event_date': formData.get('event-date'),
+    'event_name': formData.get('event-name'),
+    'description': formData.get('post-content'),
+    'date_posted': formData.get('date-posted')
   };
   try {
-    const response = await createPost(postData);
-    console.log(response);
-    // Display all the posts
-    const postContainer = document.querySelector('#post-container');
-    const posts = await getAllPosts();
-    posts.forEach(post => {
-      const postElement = document.createElement('div');
-      postElement.innerHTML = `
-        <h2>${post['event-name']}</h2>
-        <p>${post['post-content']}</p>
-        <small>Posted for ${post['artist-name']} on ${post['date-posted']}</small>
-      `;
-      postContainer.appendChild(postElement);
-    });
+    await crud.crudCreatePost(postData)
     // Redirect to the post page
     window.location.href = REDIRECT_URI;
   } catch (error) {
--- client/js/groups.js
+++ client/js/groups.js
@@ -1,5 +1,4 @@
 import * as crud from "./crud.js";
-//import * as forumCrud from "./forumCrud"
 
 //access the groups.html page
 //get data from crud for forums
@@ -8,9 +7,6 @@ import * as crud from "./crud.js";
 //[{},{},{},{},{}]
 
 let forums = document.getElementById("forums");
-
-console.log("in groups befroe the crud now whaaaattttt");
-
 await crud.crudCreatePost({
   artistName: "Anvesh Sunkara",
   eventDate: "2023-07-30",
@@ -18,21 +14,12 @@ await crud.crudCreatePost({
   Description: "Anvesh is performing",
   DatePosted: "2023-04-23",
 });
-await crud.crudCreatePost({"artistName": "Aayush Patel", "eventDate": "2023-11-05", "eventName": "Ultra Miami", "Description": "Aayush is performing", "DatePosted": "2023-04-24"});
-await crud.crudCreatePost({"artistName": "Arnav Kolli", "eventDate": new Date("2023-04-12"), "eventName": "Rolling Loud NYC", "Description": "Arnav is performing", "DatePosted": new Date("2023-04-25")});
-await crud.crudCreatePost({"artistName": "Rahul Vedula", "eventDate": new Date("2023-08-02"), "eventName": "GovBall", "Description": "Rahul is performing", "DatePosted": new Date("2023-04-26")});
-console.log("in groups befroe the readALl now whaaaattttt");
 
 async function main() {
   let posts = await crud.crudReadAllPosts();
-  console.log("posts", posts);
-
-  //posts = await *insert function to retrieve posts*
-  //json parse this to get an array
-  // let posts = [{}]
+  console.log("posts", posts)
   let coolData = posts.data;
   for (let i = 0; i < coolData.length; i++) {
-    console.log("coolkid lamoooooooooo") //test
     if (coolData[i].hasOwnProperty("artist_name")) {
       let post = document.createElement("div");
       post.id = coolData[i]["id"];
